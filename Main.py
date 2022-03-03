eb26ccc74e76447f71eec91d0222abb5fec726ce
DD-MBA-01 <adminlokal@DD-MBA-01.fritz.box>
2022-03-03 20:46:54 +0100

    Added structure to main.py

--- Main.py
+++ Main.py
@@ -2,17 +2,281 @@ from Operations.Addition import *
 
 whack = addition()
 
-inputnumbertype1 = input(
-    "What numbersystem is the first number? (Bin / Okt / Dez / Hex):")
+inputnumbertype1 = input("What numbersystem is the first number? (Bin / Okt / Dez / Hex):")
+inputnumbertype1.lower()
 
-if inputnumbertype1 == "Bin":
+if inputnumbertype1 == "bin":
 
     number1 = int(input("Enter number 1:"))
-    inputnumbertype2 = input(
-        "What numbersystem is the second number? (Bin/Okt/Dez/Hex):")
+    inputnumbertype2 = input("What numbersystem is the second number? (Bin/Okt/Dez/Hex):")
 
-    if inputnumbertype2 == "Bin":
+    if inputnumbertype2 == "bin":
         number2 = int(input("Enter number 2:"))
         operator = input("Enter an operator ( + / - / * / /):")
 
         if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
+
+    if inputnumbertype2 == "okt":
+        number2 = int(input("Enter number 2:"))
+        operator = input("Enter an operator ( + / - / * / /):")
+
+        if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
+
+    if inputnumbertype2 == "dez":
+        number2 = int(input("Enter number 2:"))
+        operator = input("Enter an operator ( + / - / * / /):")
+
+        if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
+
+    if inputnumbertype2 == "hex":
+        number2 = int(input("Enter number 2:"))
+        operator = input("Enter an operator ( + / - / * / /):")
+
+        if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
+
+if inputnumbertype1 == "okt":
+
+    number1 = int(input("Enter number 1:"))
+    inputnumbertype2 = input("What numbersystem is the second number? (Bin/Okt/Dez/Hex):")
+
+    if inputnumbertype2 == "bin":
+        number2 = int(input("Enter number 2:"))
+        operator = input("Enter an operator ( + / - / * / /):")
+
+        if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
+
+    if inputnumbertype2 == "okt":
+        number2 = int(input("Enter number 2:"))
+        operator = input("Enter an operator ( + / - / * / /):")
+
+        if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
+
+    if inputnumbertype2 == "dez":
+        number2 = int(input("Enter number 2:"))
+        operator = input("Enter an operator ( + / - / * / /):")
+
+        if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
+
+    if inputnumbertype2 == "hex":
+        number2 = int(input("Enter number 2:"))
+        operator = input("Enter an operator ( + / - / * / /):")
+
+        if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
+
+if inputnumbertype1 == "dez":
+
+    number1 = int(input("Enter number 1:"))
+    inputnumbertype2 = input("What numbersystem is the second number? (Bin/Okt/Dez/Hex):")
+
+    if inputnumbertype2 == "bin":
+        number2 = int(input("Enter number 2:"))
+        operator = input("Enter an operator ( + / - / * / /):")
+
+        if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
+
+    if inputnumbertype2 == "okt":
+        number2 = int(input("Enter number 2:"))
+        operator = input("Enter an operator ( + / - / * / /):")
+
+        if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
+
+    if inputnumbertype2 == "dez":
+        number2 = int(input("Enter number 2:"))
+        operator = input("Enter an operator ( + / - / * / /):")
+
+        if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
+
+    if inputnumbertype2 == "hex":
+        number2 = int(input("Enter number 2:"))
+        operator = input("Enter an operator ( + / - / * / /):")
+
+        if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
+
+if inputnumbertype1 == "hex":
+
+    number1 = int(input("Enter number 1:"))
+    inputnumbertype2 = input("What numbersystem is the second number? (Bin/Okt/Dez/Hex):")
+
+    if inputnumbertype2 == "bin":
+        number2 = int(input("Enter number 2:"))
+        operator = input("Enter an operator ( + / - / * / /):")
+
+        if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
+
+    if inputnumbertype2 == "okt":
+        number2 = int(input("Enter number 2:"))
+        operator = input("Enter an operator ( + / - / * / /):")
+
+        if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
+
+    if inputnumbertype2 == "dez":
+        number2 = int(input("Enter number 2:"))
+        operator = input("Enter an operator ( + / - / * / /):")
+
+        if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
+
+    if inputnumbertype2 == "hex":
+        number2 = int(input("Enter number 2:"))
+        operator = input("Enter an operator ( + / - / * / /):")
+
+        if operator == "+":
+            print("Your result is:")
+
+        if operator == "-":
+            print("Your result is:")
+        
+        if operator == "*":
+            print("Your result is:")
+        
+        if operator == "/":
+            print("Your result is:")
---
+++ ToDez/ToDez.py
@@ -0,0 +1,6 @@
+class toDez:
+    def __init__(self):
+        print("Initialized")
+
+    def BinToDez(self, number):
+        
